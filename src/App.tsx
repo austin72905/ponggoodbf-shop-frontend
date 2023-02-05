2399ebe6ae0c39ff5c79a7d19a327637add3fb7a
austin72905 <Linponggood@gmail.com>
2023-02-05 18:44:42 +0800

    新增聊天室頁面

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,16 @@ import { orange } from '@mui/material/colors';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import Divider from '@mui/material/Divider';
+import InputBase from '@mui/material/InputBase';
 import { styled, Stack } from '@mui/system';
 import IconButton from '@mui/material/IconButton';
+import EmailIcon from '@mui/icons-material/Email';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SendIcon from '@mui/icons-material/Send';
 
 import RoutesRegister from './routesRegister/RoutesRegister'
 import ScrollToTop from "./components/ScrollToTop";
@@ -25,8 +32,16 @@ import InstagramImageBlack from './assets/iconmonstr-instagram-11.svg'
 import InstagramImage from './assets/Instagram_logo_2016.svg'
 import SvgIcon from '@mui/material/SvgIcon';
 import Icon from '@mui/material/Icon';
+import { Paper } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 
-
+import LogoImage from './assets/朋朋大頭貼.jpg'
 
 const customTheme = createTheme({
   palette: {
@@ -44,7 +59,9 @@ function App() {
 
   const [instgramIsHover, setInstgramIsHover] = useState(InstagramImageBlack)
 
-  const {pathname}=useLocation()
+  const { pathname } = useLocation()
+
+  const [chatOpen, setChatOpen] = useState(false)
 
 
   return (
@@ -52,9 +69,9 @@ function App() {
       <Box sx={{ minHeight: "100vh", border: "0px solid", backgroundColor: "#fefefe" }}>
         <TopBar />
         <Toolbar />
-        <ScrollToTop/>
+        <ScrollToTop />
         <Container sx={{ marginTop: "28px", border: "0px solid" }}>
-          <Stack direction="row" spacing={3} divider={(pathname.includes("/login") ||pathname.includes("/signup"))?null: <Divider orientation="vertical" flexItem />}>
+          <Stack direction="row" spacing={3} divider={(pathname.includes("/login") || pathname.includes("/signup")) ? null : <Divider orientation="vertical" flexItem />}>
             <SideBar routeList={routeList} routeAccountList={routeAccountList} />
             <Box sx={{ width: "100%" }}>
               <RoutesRegister routeList={routeList} routeAccountList={routeAccountList} />
@@ -69,7 +86,7 @@ function App() {
             <footer>
               <Stack direction={"row"} alignItems={"center"} justifyContent={"center"} sx={{ m: "20px" }} spacing={"15px"}>
                 <p>關於我們</p>
-                <IconButton sx={{ border: "1px solid #e5e5e5",width: "40px", height: "40px" }} onMouseEnter={() => setInstgramIsHover(s => InstagramImage)} onMouseLeave={() => setInstgramIsHover(s => InstagramImageBlack)}>
+                <IconButton sx={{ border: "1px solid #e5e5e5", width: "40px", height: "40px" }} onMouseEnter={() => setInstgramIsHover(s => InstagramImage)} onMouseLeave={() => setInstgramIsHover(s => InstagramImageBlack)}>
                   <Icon >
                     <img src={instgramIsHover} style={{ width: "25px", height: "25px" }} />
                   </Icon>
@@ -94,6 +111,124 @@ function App() {
         </Container>
 
       </Box>
+      <Box sx={{ bottom: "0px", right: "10px", border: "1px solid #9c9c9c", position: "fixed", zIndex: 9999 }} >
+        <Paper >
+          <Stack onClick={() => { setChatOpen(s => !s) }} justifyContent={"space-between"} direction={"row"} sx={{ background: "#61D1BD", border: "1px solid #9c9c9c", width: "300px", height: "40px", alignItems: "center" }} >
+            <Stack sx={{ border: "0px solid #9c9c9c" }} direction={"row"} >
+              <ChatOutlinedIcon style={{ color: 'white' }} sx={{ width: "30px", height: "30px", pl: "15px" }} />
+              <Typography variant='h5' sx={{ pl: "15px", letterSpacing: "5px", color: "white" }}>聯絡賣家</Typography>
+            </Stack>
+
+            <Box sx={{ mr: "10px" }} >
+              {chatOpen ?
+                <ArrowDropDownIcon sx={{ color: "white", "&:hover": { color: "black" } }} />
+                :
+                <ArrowDropUpIcon sx={{ color: "white", "&:hover": { color: "black" } }} />
+              }
+
+
+            </Box>
+
+
+          </Stack>
+
+          <Stack justifyContent={"space-between"} sx={{ height: "400px", display: chatOpen ? "flex" : "none" }}>
+            {/*聊天紀錄 */}
+            <Box sx={{ height: "350px" }}>
+              <List sx={{ maxWidth: "300px", maxHeight: "350px", overflow: 'auto' }}>
+                <ListItem  sx={{py: "1px",px:"10px",border: "0px solid #d9d9d9"}}>
+                  
+                  <ListItemText sx={{border: "0px solid #d9d9d9"}} primary={<Stack direction={"row"} justifyContent={"center"}><Typography variant='caption'>2023/2/5 下午06:29</Typography></Stack>}/>
+                 
+                  
+                </ListItem>
+                <ListItem alignItems="flex-start" sx={{ border: "0px solid #d9d9d9", py: "1px",px:"10px" }}>
+                  <ListItemAvatar sx={{ '&.MuiListItemAvatar-root': { minWidth: "40px" }, border: "0px solid #d9d9d9", mr: "5px" }}>
+                    <Avatar alt="LogoImage" src={LogoImage} />
+                  </ListItemAvatar>
+                  <Stack spacing={"2px"} direction={"row"} sx={{ border: "0px solid #d9d9d9" }}>
+
+                    <ListItemText sx={{ py: "6px", px: "10px", border: "1px solid #d9d9d9", borderRadius: "20px" }} primary={<Typography variant='subtitle2' sx={{ border: "0px solid #d9d9d9" }} >請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?請問有下單嗎?</Typography>} />
+                   
+                  </Stack>
+
+
+
+                </ListItem>
+
+                <ListItem sx={{ border: "0px solid #d9d9d9", display: "flex", flexDirection: "row", justifyContent: "end", py: "1px",px:"10px" }}>
+                  <Box sx={{ border: "0px solid #d9d9d9" }}>
+                    <ListItemText sx={{ py: "6px", px: "10px", backgroundColor: "#96DB8B", border: "1px solid #d9d9d9", borderRadius: "20px" }} primary={<Typography variant='subtitle2' sx={{ border: "0px solid #d9d9d9" }} >是的，請問何時出貨?</Typography>} />
+                  </Box>
+
+                </ListItem>
+                <ListItem sx={{ border: "0px solid #d9d9d9", display: "flex", flexDirection: "row", justifyContent: "end", py: "1px",px:"10px" }}>
+                  <Box sx={{ border: "0px solid #d9d9d9" }}>
+                    <ListItemText sx={{ py: "6px", px: "10px", backgroundColor: "#96DB8B", border: "1px solid #d9d9d9", borderRadius: "20px" }} primary={<Typography variant='subtitle2' sx={{ border: "0px solid #d9d9d9" }} >是的，請問何時出貨?</Typography>} />
+                  </Box>
+
+                </ListItem>
+                <ListItem sx={{ border: "0px solid #d9d9d9", display: "flex", flexDirection: "row", justifyContent: "end", py: "1px",px:"10px" }}>
+                  <Box sx={{ border: "0px solid #d9d9d9" }}>
+                    <ListItemText sx={{ py: "6px", px: "10px", backgroundColor: "#96DB8B", border: "1px solid #d9d9d9", borderRadius: "20px" }} primary={<Typography variant='subtitle2' sx={{ border: "0px solid #d9d9d9" }} >是的，請問何時出貨?</Typography>} />
+                  </Box>
+
+                </ListItem>
+                <ListItem sx={{ border: "0px solid #d9d9d9", display: "flex", flexDirection: "row", justifyContent: "end", py: "1px",px:"10px" }}>
+                  <Box sx={{ border: "0px solid #d9d9d9" }}>
+                    <ListItemText sx={{ py: "6px", px: "10px", backgroundColor: "#96DB8B", border: "1px solid #d9d9d9", borderRadius: "20px" }} primary={<Typography variant='subtitle2' sx={{ border: "0px solid #d9d9d9" }} >是的，請問何時出貨?</Typography>} />
+                  </Box>
+
+                </ListItem>
+
+                <ListItem  sx={{py: "1px",px:"10px",border: "0px solid #d9d9d9"}}>
+                  
+                  <ListItemText sx={{border: "0px solid #d9d9d9"}} primary={<Stack direction={"row"} justifyContent={"center"}><Typography variant='caption'>2023/2/8 下午01:13</Typography></Stack>}/>
+                 
+                  
+                </ListItem>
+
+                <ListItem alignItems="flex-start" sx={{ border: "0px solid #d9d9d9", py: "1px",px:"10px" }}>
+                  <ListItemAvatar sx={{ '&.MuiListItemAvatar-root': { minWidth: "40px" }, border: "0px solid #d9d9d9", mr: "5px" }}>
+                    <Avatar alt="LogoImage" src={LogoImage} />
+                  </ListItemAvatar>
+                  <Stack spacing={"2px"} direction={"row"} sx={{ border: "0px solid #d9d9d9" }}>
+
+                    <ListItemText sx={{ py: "6px", px: "10px", border: "1px solid #d9d9d9", borderRadius: "20px" }} primary={<Typography variant='subtitle2' sx={{ border: "0px solid #d9d9d9" }} >付款完就會出貨</Typography>} />
+                   
+                  </Stack>
+
+                </ListItem>
+
+                <ListItem sx={{ border: "0px solid #d9d9d9", display: "flex", flexDirection: "row", justifyContent: "end", py: "1px",px:"10px" }}>
+                  <Box sx={{ border: "0px solid #d9d9d9" }}>
+                    <ListItemText sx={{ py: "6px", px: "10px", backgroundColor: "#96DB8B", border: "1px solid #d9d9d9", borderRadius: "20px" }} primary={<Typography variant='subtitle2' sx={{ border: "0px solid #d9d9d9" }} >那麼晚才回是不想做生意了?</Typography>} />
+                  </Box>
+
+                </ListItem>
+
+              </List>
+            </Box>
+
+            <Stack direction={"row"} sx={{ justifyContent: "end" }}>
+              <Paper sx={{ border: "1px solid #d9d9d9", boxShadow: "none", display: 'flex', alignItems: 'center', width: "300px", height: 35 }}>
+
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="輸入訊息"
+                  inputProps={{ 'aria-label': '輸入訊息' }}
+                />
+
+                <SendIcon sx={{ p: '10px', width: "20px", height: "20px", borderRadius: "0px", '&:hover': { cursor: "pointer" } }} aria-label="send" />
+
+              </Paper>
+            </Stack>
+          </Stack>
+
+        </Paper>
+
+      </Box>
+
 
     </ThemeProvider>
   );
