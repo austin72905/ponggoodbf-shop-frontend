fdee20976b47c4087b83789a2e97e2f0061be843
austin72905 <Linponggood@gmail.com>
2023-02-12 17:20:26 +0800

    登入、註冊頁面統一距離

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -17,52 +17,49 @@ import Checkbox from '@mui/material/Checkbox';
 
 export default function Login() {
 
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const toSignUp=()=>{
+    const toSignUp = () => {
         navigate("/signup")
     }
     return (
         <Container sx={{ border: "0px solid" }} maxWidth='xl'>
-            <Stack justifyContent={"center"} direction={"row"} sx={{ border: "1px solid #9f9f9f", mt: "30px",borderRadius:"4px" }}>
+            <Stack justifyContent={"center"} direction={"row"} sx={{ border: "1px solid #9f9f9f", mt: 4, borderRadius: "4px" }}>
 
-                <Stack spacing={"20px"} sx={{ border: "0px solid", minHeight: "600px", minWidth: "400px" }} justifyContent={"center"}>
+                <Stack spacing={2.5} sx={{ border: "0px solid", minHeight: "600px", minWidth: "400px" }} justifyContent={"center"}>
                     <Stack alignItems={"center"}>
                         <Typography variant='h6' sx={{ fontWeight: 'bold' }}>會員登入</Typography>
                     </Stack>
 
+                    <InputSet label='帳號' placeholder='請輸入帳號' />
 
-                    <ItemWrapper>
-                        <Typography variant='body1' >帳號</Typography>
-                        <TextField placeholder='請輸入帳號' sx={{ marginTop: "10px" }} autoComplete='off' fullWidth />
-                    </ItemWrapper>
-
-                    <ItemWrapper>
+                    <Stack spacing={1} sx={{ pt: 1 }}>
                         <Typography variant='body1' >密碼</Typography>
-                        <TextField placeholder='請輸入密碼' sx={{ marginTop: "10px" }} autoComplete='off' fullWidth />
-                        <Stack sx={{ mt: "8px",border: "0px solid"}} direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-
-                            <FormControlLabel sx={{ ml:"1px",border: "0px solid"  }} control={<Checkbox sx={{ border: "0px solid", width: "25px", height: "25px" }} />} label={<Typography sx={{px:"5px"}} variant='subtitle2'>記住我</Typography>} />
-
+                        <TextField placeholder={'請輸入密碼'} autoComplete='off' fullWidth />
+                        <Stack sx={{ mt: 1 }} direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+                            <FormControlLabel
+                                sx={{ ml: "1px" }}
+                                control={<Checkbox sx={{ width: "25px", height: "25px" }} />}
+                                label={<Typography sx={{ px: 1 }} variant='subtitle2'>記住我</Typography>} />
 
                             <Typography variant='body2' sx={{ color: "#3E8FB2" }}>忘記密碼?</Typography>
                         </Stack>
+                    </Stack>
+                    
+                    <Stack spacing={1} sx={{ pt: 1 }}>
+                    <Button  variant='contained' size='large' fullWidth>登入</Button>
+                    </Stack>
+                    
 
-                    </ItemWrapper>
-
-                    <ItemWrapper>
-                        <Button sx={{ marginTop: "10px" }} variant='contained' size='large' fullWidth>登入</Button>
-                    </ItemWrapper>
-
-                    <ItemWrapper>
-                        <Stack spacing={"10px"} direction={"row"}>
-                            <Typography variant='body2' sx={{color:"#9f9f9f"}}>還沒有帳號嗎?</Typography>
-                            <Typography variant='body2' sx={{ color: "#3E8FB2",cursor:"pointer" }} onClick={toSignUp}>註冊新會員</Typography>
+                    <Stack  sx={{ pt: 1 }}>
+                        <Stack spacing={1} direction={"row"}>
+                            <Typography variant='body2' sx={{ color: "#9f9f9f" }}>還沒有帳號嗎?</Typography>
+                            <Typography variant='body2' sx={{ color: "#3E8FB2", cursor: "pointer" }} onClick={toSignUp}>註冊新會員</Typography>
                         </Stack>
 
-                    </ItemWrapper>
+                    </Stack>
 
-
+                    
                 </Stack>
 
 
@@ -79,3 +76,21 @@ const ItemWrapper = styled(Box)({
     paddingTop: "5px",
 
 })
+
+
+interface InputSetProps {
+    label: string;
+    placeholder: string;
+}
+
+const InputSet = ({ label, placeholder }: InputSetProps) => {
+
+
+    return (
+        <Stack spacing={1} sx={{ pt: 1 }}>
+            <Typography variant='body1' >{label}</Typography>
+            <TextField placeholder={placeholder} autoComplete='off' fullWidth />
+        </Stack>
+    )
+
+}
--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -23,54 +23,37 @@ export default function SignUp() {
   }
   return (
     <Container sx={{ border: "0px solid" }} maxWidth='xl'>
-      <Stack justifyContent={"center"} direction={"row"} sx={{ border: "1px solid #9f9f9f", mt: "30px", borderRadius: "4px" }}>
+      <Stack justifyContent={"center"} direction={"row"} sx={{ border: "1px solid #9f9f9f", mt: 4, borderRadius: "4px" }}>
 
-        <Stack spacing={"20px"} sx={{ border: "0px solid", minHeight: "800px", minWidth: "400px",my:"30px" }} justifyContent={"center"}>
+        <Stack spacing={2.5} sx={{ border: "0px solid", minHeight: "800px", minWidth: "400px", my: 4 }} justifyContent={"center"}>
+          
           <Stack alignItems={"center"}>
             <Typography variant='h6' sx={{ fontWeight: 'bold' }}>註冊新會員</Typography>
           </Stack>
 
-          <ItemWrapper >
-            <Typography variant='body1' >姓名</Typography>
-            <TextField placeholder='不得包含特殊符號 / $ . @ & # @...'  sx={{ marginTop: "10px" }}  fullWidth />
-          </ItemWrapper>
-         
-          <ItemWrapper>
-            <Typography variant='body1' >信箱</Typography>
-            <TextField placeholder='ex: asbc@gmail.com'  sx={{ marginTop: "10px" }}  fullWidth />
-          </ItemWrapper>
+          <InputSet label='姓名' placeholder='不得包含特殊符號 / $ . @ & # @...' />
 
+          <InputSet label='信箱' placeholder='ex: asbc@gmail.com' />
 
-          <ItemWrapper>
-            <Typography variant='body1' >帳號</Typography>
-            <TextField placeholder='請輸入帳號' sx={{ marginTop: "10px" }} autoComplete='off' fullWidth />
-          </ItemWrapper>
+          <InputSet label='帳號' placeholder='請輸入帳號' />
 
-          <ItemWrapper>
-            <Typography variant='body1' >密碼</Typography>
-            <TextField placeholder='請輸入密碼' sx={{ marginTop: "10px" }} autoComplete='off' fullWidth />
-          </ItemWrapper>
+          <InputSet label='密碼' placeholder='請輸入密碼' />
 
-          <ItemWrapper >
-                    <Typography variant='body1' >密碼確認</Typography>
-                    <TextField placeholder='請再次確認輸入密碼'  sx={{ marginTop: "10px" }}  fullWidth />
-                </ItemWrapper>
+          <InputSet label='密碼確認' placeholder='請再次確認輸入密碼' />
 
-          <ItemWrapper>
-            <Button sx={{ marginTop: "10px" }} variant='contained' size='large' fullWidth>加入會員</Button>
-          </ItemWrapper>
+          <Stack sx={{ pt: 1 }}>
+            <Button variant='contained' size='large' fullWidth>加入會員</Button>
+          </Stack>
 
-          <ItemWrapper>
-            <Stack spacing={"10px"} direction={"row"}>
+          <Stack sx={{ pt: 1 }}>
+            
+            <Stack spacing={1} direction={"row"}>
               <Typography variant='body2' sx={{ color: "#9f9f9f" }}>已經有會員了?</Typography>
               <Typography variant='body2' sx={{ color: "#3E8FB2", cursor: "pointer" }} onClick={toLogin}>登入</Typography>
             </Stack>
-
-          </ItemWrapper>
-
+          </Stack>
 
         </Stack>
-
 
 
       </Stack>
@@ -85,3 +68,21 @@ const ItemWrapper = styled(Box)({
   paddingTop: "5px",
 
 })
+
+
+interface InputSetProps {
+  label: string;
+  placeholder: string;
+}
+
+const InputSet = ({ label, placeholder }: InputSetProps) => {
+
+
+  return (
+    <Stack spacing={1} sx={{ pt: 1 }}>
+      <Typography variant='body1' >{label}</Typography>
+      <TextField placeholder={placeholder} autoComplete='off' fullWidth />
+    </Stack>
+  )
+
+}
